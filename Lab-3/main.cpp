d52abf546297ba0ec8de6dbea57bfc17ee0ad087
Semyon <s_se@ngs.ru>
2020-04-06 23:08:41 +0700

    Update main.cpp

--- Lab-3/main.cpp
+++ Lab-3/main.cpp
@@ -1,15 +1,21 @@
+#define TEST
+
 #include <iostream>
 #include <string>
 #include <random>
+#include <vector>
 #include <array>
 
 using namespace std;
 
-constexpr time_t g_totalTime       = 15;
-//constexpr time_t g_totalTime   = 86400;
-constexpr int    g_numIterations   = 5;
-constexpr int    g_maxFileSize     = 10;
-constexpr time_t g_maxInterval     = 10;
+#ifdef TEST
+constexpr time_t g_totalTime         = 15;
+#else
+constexpr time_t g_totalTime         = 86400;
+#endif
+constexpr int    g_numIterations     = 5;
+constexpr int    g_maxFileSize       = 10;
+constexpr time_t g_maxInterval       = 10;
 
 int64_t randR(int64_t _from, int64_t _to) {
 	static random_device rd;
@@ -17,6 +23,7 @@ int64_t randR(int64_t _from, int64_t _to) {
 	return (rd() % (_to - _from + 1) + _from);
 }
 
+/* задержка перед отправкой нового файла */
 class Delay {
 private:
 	const time_t m_maxInterval;
@@ -43,6 +50,7 @@ time_t getNewDelay(Delay& _delay) {
 	return _delay.m_delayTime;
 }
 
+/* сам файл */
 class Files {
 private:
 	const int m_maxSize{};
@@ -62,6 +70,7 @@ int getNewFile(Files& _file) {
 	return _file.m_size;
 }
 
+/* Память, которая хранит в себе файлы посегментно */
 class Memory {
 private:
 	int m_segmentsCount{};
@@ -89,15 +98,90 @@ public:
 
 };
 
+/* структура для коллекционирования данных в итоговую таблицу */
 struct InfoNode {
 	uint32_t maxLength{};
 	double_t mean{};	
 
 	InfoNode(double_t _mean = 0, uint32_t _maxLength = 0) {
-		maxLength = _maxLength;
 		mean = _mean;
+		maxLength = _maxLength;
 	}
 };
+
+///* работа с графиком */
+//class Chart {
+//public:
+//	static constexpr int length = 20;
+//	static constexpr int interval = g_totalTime / length;
+//	static constexpr int heigth = 25;
+//
+//	struct Node;
+//	using  chart_t = std::vector<Node>;
+//
+//private:
+//	int      _maxBufferSize{};
+//	chart_t  _chart;
+//
+//	/* структура для коллекционирования значений в график*/
+//	struct Node {
+//		time_t  time{};
+//		int     length{};
+//
+//		Node(time_t _time, int _length) : time(_time), length(_length) {}
+//	};
+//
+//public:
+//	int pushNode(time_t _time, int _bufferSize) {
+//		if (length < 1) return 0;
+//
+//		if (_bufferSize > _maxBufferSize) _maxBufferSize = _bufferSize;
+//
+//		if (_time == 0 || _time == g_totalTime - 1 || interval <= 1 || _time % interval == 0) {
+//			_chart.push_back(Node(_time, _bufferSize));
+//			return 1;
+//		}
+//
+//		return 0;
+//	}
+//
+//	void out(void) {
+//		vector<string> outMatrix;
+//		vector<int> raws;
+//
+//		if (_maxBufferSize < heigth) {
+//			outMatrix.resize(_maxBufferSize + 2Ui64);
+//			raws.reserve(outMatrix.size());
+//
+//			for (auto& str : outMatrix) {
+//				static int i = _maxBufferSize;
+//				raws.push_back(i);
+//				str += to_string(i--) += "\t";
+//				if (i == -1) break;
+//			}
+//			raws.push_back(-1);
+//
+//			string& lastStr = outMatrix.at(outMatrix.size() - 1);
+//			lastStr += "\t";
+//			for (size_t i = 0; i < _chart.size(); ++i) {
+//				lastStr += to_string(_chart.at(i).time) + "\t";				
+//			}
+//
+//			for (auto node : _chart) {
+//				for (size_t i = 0; i < outMatrix.size() - 1; ++i) {
+//					if (node.length == raws.at(i)) {
+//						outMatrix.at(i) += "█";
+//					}
+//					outMatrix.at(i) += "\t";
+//				}
+//			}
+//
+//			for (auto str : outMatrix) {
+//				cout << str << endl;
+//			}
+//		}
+//	}
+//};
 
 int main() {
 	system("chcp 65001"); system("cls");
@@ -107,7 +191,7 @@ int main() {
 		Memory RAM;
 		Files file(g_maxFileSize);
 		Delay delay(g_maxInterval);
-
+		
 		cout << "Период\t\t" << "Количество сегментов\t\t\t\t" << "Максимальное\t" << "Среднее" << endl;
 
 		uint64_t totalLength{};
@@ -133,7 +217,7 @@ int main() {
 			
 			cout << outStr << endl;
 		}
-		collector[iterator] = InfoNode(static_cast<double>(totalLength) / g_totalTime, totalLength);
+		collector.at(iterator) = InfoNode(static_cast<double>(totalLength) / g_totalTime, totalLength);
 
 		cout << "[TIME OUT]" << endl;
 		cout << "Максимальный объём занимаемой памяти: " << RAM.getMaxSegmentsCount() << endl << endl;
@@ -147,11 +231,11 @@ int main() {
 	}
 	cout << endl << "Среднее\t";
 	for (int i = 0; i < g_numIterations; ++i) {
-		cout << collector[i].mean << "\t\t";
+		cout << collector.at(i).mean << "\t\t";
 	}
 	cout << endl << "Макс.\t";
 	for (int i = 0; i < g_numIterations; ++i){
-		cout << collector[i].maxLength << "\t\t";
+		cout << collector.at(i).maxLength << "\t\t";
 	}
 	cout << endl;
 
